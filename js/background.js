dae9641bd14cb21e94d97cd75ee5d984c4645032
mimoria <mimoria12@gmail.com>
2020-10-01 19:29:37 +0900

    aa

--- js/background.js
+++ js/background.js
@@ -1,9 +1,7 @@
 window.onload = function() {
-  Particles.init({
-    selector: '.background',
-    sizeVariations: 50,
-    color: [
-      '#0bd', 'rgba(0,187,221,.5)', 'rgba(0,187,221,.2)'
-    ]
+  bubbly({
+    colorStart: '#4c004c',
+    colorStop: '#1a001a',
+    bubbleFunc:() => `hsla(${Math.random() * 360}, 100%, 50%, ${Math.random() * 0.25})`
   });
 };
