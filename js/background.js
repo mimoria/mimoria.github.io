d23c14213dec8472e0d36d9c85c843c4b56386be
mimoria <mimoria12@gmail.com>
2020-10-01 22:24:10 +0900

    commit

--- js/background.js
+++ js/background.js
@@ -5,8 +5,18 @@ window.onload = function() {
     bubbleFunc:() => `hsla(${Math.random() * 360}, 100%, 50%, ${Math.random() * 0.25})`
   });
   new Typed('.mainText', {
-    strings: ['To M^1000\n\n いつもお世話になっております。^1000\n\n From H^1000'],
-    typeSpeed: 40,
+    strings: ['Dear M^1000\n\n' +
+      '24才の誕生日おめでとう😊^1000\n' +
+      'いつも一緒に遊んでくれて\n' +
+      'ありがとうございます(笑)🙇^1000\n' +
+      '今年も誕生日を一緒にお祝いできて\n' +
+      'とても嬉しいです！！^1000\n' +
+      'これから仕事も忙しくなるかもだけど\n' +
+      'あまり無理をしないようにね✋^1000\n' +
+      '何かあったら何でも言ってねー(^-^)/^1000\n' +
+      'ではでは　良い一日を👍^1000\n' +
+      '\nFrom H'],
+    typeSpeed: 50,
     backSpeed: 0,
     loop: false
   });
