cf0aa84f89f348075d8688587bbfa62884ac4c5b
mimoria <mimoria12@gmail.com>
2020-10-01 21:18:41 +0900

    aaaa

--- js/background.js
+++ js/background.js
@@ -4,4 +4,10 @@ window.onload = function() {
     colorStop: '#1a001a',
     bubbleFunc:() => `hsla(${Math.random() * 360}, 100%, 50%, ${Math.random() * 0.25})`
   });
+  new Typed('.mainText', {
+    strings: ['To M^1000\n\n いつもお世話になっております。^1000\n\n From H^1000'],
+    typeSpeed: 40,
+    backSpeed: 0,
+    loop: false
+  });
 };
